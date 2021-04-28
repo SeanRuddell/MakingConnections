4c8a61667ef193980fe3a47c928d0b4cc8467f54
SeanRuddell <33820035+SeanRuddell@users.noreply.github.com>
2021-04-28 00:00:30 -0700

    Add files via upload

---
+++ MakingConnections/script.js
@@ -0,0 +1,12 @@
+var x = document.querySelector(".cursive");
+function changename(){
+    x.innerHTML = "Shadow Master"
+}
+var y = document.querySelector(".row")
+function gone(){
+    y.style.display = 'none';
+}
+var z = document.querySelector(".row2")
+function gone2(){
+    z.style.display = 'none';
+}
